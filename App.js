e1f8df2afa46ae080e05af8b90737dee4c5a627c
Rudra Pratap <1000011653@dit.edu.in>
2025-06-25 23:43:30 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';  
+   
+   
    /*
 <div id="parent">
 <div id="child">
@@ -11,23 +15,13 @@
 </div>
 ReactElement(object) => HTML(browser understands)
 */
-
-const parent=React.createElement('div',{id:"parent"},[
-            React.createElement( 'div',  {id:"child"},[
-            React.createElement('h1', {}, 'i am h1 tag'),
-            React.createElement('h2', {}, 'i am h2 tag'),
- ] ),
-            React.createElement( 'div', {id:"child2"},[
-            React.createElement('h1', {}, 'i am a heading'),
-            React.createElement('h2', {}, 'i am a Tag'),
- ]),
-   ]); 
-   
-   
-console.log(parent);
-    const root=ReactDOM.createRoot(document.getElementById('root'));
-    root.render(parent);
-
-
-
-
+const parent = React.createElement('div', { id: "parent" }, [
+  React.createElement('div', { id: "child", key: "child1" }, [
+    React.createElement('h1', {}, 'i am h1 tag'),
+    React.createElement('h2', {}, 'i am h2 tag'),
+  ]),
+  React.createElement('div', { id: "child2", key: "child2" }, [
+    React.createElement('h1', {}, 'i am a heading'),
+    React.createElement('h2', {}, 'i am a Tag'),
+  ]),
+]);
